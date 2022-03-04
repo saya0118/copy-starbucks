a6d7a530d30d98eda38279016028f6c22b3aa68c
saya0118 <chemi0802@gmail.com>
2022-03-04 12:50:11 -0800

    Initialize repo

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css'
+
+const Button = (props) => {
+    return(
+        <button className={props.className}>{props.text}</button>
+    )
+}
+
+export default Button;
